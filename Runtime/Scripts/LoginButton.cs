e1f7e662c617f83f13bf9a925d5f3f23283d6582
Alex Dunn <alexander.dunn@outlook.com>
2026-05-13 08:15:41 -0700

    add namespace

--- Runtime/Scripts/LoginButton.cs
+++ Runtime/Scripts/LoginButton.cs
@@ -2,42 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class LoginButton : MonoBehaviour
+namespace Cephable.Unity.Plugin
 {
-    public OAuth2Manager oauth2Manager;
-    public Button loginButton;
-
-    private void Start()
+    public class LoginButton : MonoBehaviour
     {
-        loginButton.onClick.AddListener(OnLoginButtonClick);
-        // if we have a stored access token, we are already logged in
-        var accessToken = PlayerPrefs.GetString("accessToken");
-        if (!string.IsNullOrEmpty(accessToken))
-        {
-            // set the login button text to say sign out
-            loginButton.GetComponentInChildren<Text>().text = "Sign Out";
-        }
-        else
-        {
-            loginButton.GetComponentInChildren<Text>().text = "Sign In";
-        }
-    }
+        public OAuth2Manager oauth2Manager;
+        public Button loginButton;
 
-    public void OnLoginButtonClick()
-    {
-        // if we have an access token, delete it and sign out
-        var accessToken = PlayerPrefs.GetString("accessToken");
-        if (!string.IsNullOrEmpty(accessToken))
+        private void Start()
         {
-            oauth2Manager.Signout();
-            //loginButton.GetComponentInChildren<Text>().text = "Sign In";
-            return;
-        }
-        else
-        {
-            oauth2Manager.StartAuthorization();
+            loginButton.onClick.AddListener(OnLoginButtonClick);
+            // if we have a stored access token, we are already logged in
+            var accessToken = PlayerPrefs.GetString("accessToken");
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                // set the login button text to say sign out
+                loginButton.GetComponentInChildren<Text>().text = "Sign Out";
+            }
+            else
+            {
+                loginButton.GetComponentInChildren<Text>().text = "Sign In";
+            }
         }
 
+        public void OnLoginButtonClick()
+        {
+            // if we have an access token, delete it and sign out
+            var accessToken = PlayerPrefs.GetString("accessToken");
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                oauth2Manager.Signout();
+                //loginButton.GetComponentInChildren<Text>().text = "Sign In";
+                return;
+            }
+            else
+            {
+                oauth2Manager.StartAuthorization();
+            }
+
+        }
     }
 }
