2d2a50012daff4bc9897cd2538e1b98d0dc170c1
Alex Dunn <alexander.dunn@outlook.com>
2026-05-20 18:30:21 -0400

    sample update with moving marble and jump wired up from space command on key press from cephable

--- Cephable.Plugin/Assets/Cephable/DemoScene/BasicMovement.cs
+++ Cephable.Plugin/Assets/Cephable/DemoScene/BasicMovement.cs
@@ -1,3 +1,5 @@
+using Cephable.Plugin;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,8 @@ public class BasicMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private float horizontalInput, verticalInput;
 
+    public VirtualController CephableController;
+
     void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -15,7 +19,7 @@ public class BasicMovement : MonoBehaviour
         // Jump when space is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            Jump();
         }
 
         // Apply forces to move
@@ -23,5 +27,54 @@ public class BasicMovement : MonoBehaviour
             new Vector3(horizontalInput, 0, verticalInput) * moveSpeed,
             ForceMode.Force
         );
+    }
+
+    void OnEnable()
+    {
+        if (CephableController == null) return;
+        CephableController.MacroCommandReceived += OnMacroCommand;
+        CephableController.CustomActionReceived += OnCustomAction;
+        CephableController.OnKeyStateChanged.AddListener(OnKeyState);
+        CephableController.OnJoystickChanged.AddListener(OnJoystick);
+    }
+
+    void OnDisable()
+    {
+        if(CephableController == null) return;
+        CephableController.MacroCommandReceived -= OnMacroCommand;
+        CephableController.CustomActionReceived -= OnCustomAction;
+        CephableController.OnKeyStateChanged.RemoveListener(OnKeyState);
+        CephableController.OnJoystickChanged.RemoveListener(OnJoystick);
+    }
+
+    void OnMacroCommand(string command, MacroModel macro)
+    {
+        Debug.Log($"Command received: {command}");
+        // Handle command names like "jump", "fire", "crouch", etc.
+    }
+
+    void OnCustomAction(string deviceTypeId, string actionId, string content)
+    {
+        Debug.Log($"Custom action: {actionId}");
+    }
+
+    void OnKeyState(string key, bool isPressed)
+    {
+        Debug.Log($"Key {key} is now {(isPressed ? "pressed" : "released")}");
+
+        if(key.ToLower() == "space" && isPressed)
+        {
+            Jump();
+        }
+    }
+
+    void OnJoystick(float lx, float ly, float rx, float ry)
+    {
+        Debug.Log($"Left stick: ({lx:F2}, {ly:F2})  Right stick: ({rx:F2}, {ry:F2})");
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 5f, ForceMode.Impulse);
     }
 }
